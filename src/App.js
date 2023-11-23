ea04a3b7e36eccfbcd97476c94f0bdb740999981
Henna Maria <hennamaria2001@gmail.com>
2023-11-23 13:44:02 +0530

    text component imported

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import DarkMode from './components/DarkMode';
 import Header from './components/Header';
-// import Text from './components/Text';
+import Text from './components/Text';
 
 function App() {
   return ( //jsX starting
   <>
   <Header title='Text Utils' about='About'/>
+  <Text />
   <DarkMode />
   {/* <Text heading='Enter Your Text Here' /> */}
   {/* <Header /> */}
